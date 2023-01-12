10f6a0352ad0040689127c3a159f0f212220a8c0
Rytis <ezezke@gmail.com>
2023-01-12 20:20:32 +0200

    added builder services and cors, deleted unnecessary code

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using FinalEgzam.Database;
+using FinalEgzam.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var allowAll = "_allowAll";
+#region
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IResidenceRepository, ResidenceRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options => options.UseSqlServer
+    (builder.Configuration.GetConnectionString
+    ("Database")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowAll,
+                      build =>
+                      {
+                          build.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                      });
+});
+#endregion
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +42,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(allowAll);
 
 app.UseHttpsRedirection();
 
